3619babf225a49bfc2bfb0c8c0ccd1eef0353f7e
Jeremiah Marks <jeremiah@jlmarks.org>
2017-04-22 17:53:41 -0700

    Create basic todo item object
    Creating the basic object which will provide display options for the
    interactive text interface... totally not a java interface by the way.

--- interface/interactive_text.py
+++ interface/interactive_text.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 # @Author: Jeremiah
 # @Date:   2017-04-22 15:22:45
-# @Last Modified by:   Jeremiah
-# @Last Modified time: 2017-04-22 15:22:45
+# @Last Modified by:   Jeremiah Marks
+
+class TodoItem(object):
+	"""docstring for TodoItem"""
+		super(TodoItem, self).__init__()
+		self.itemName = itemName
