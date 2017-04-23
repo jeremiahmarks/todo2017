4f0c882916426fbcb4af353c31b32222e0737bb2
Jeremiah Marks <jeremiah@jlmarks.org>
2017-04-22 19:31:33 -0700

    define basic function to find early bugs.
    I am just making sure things load!

--- __init__.py
+++ __init__.py
@@ -2,10 +2,18 @@
 # @Author: Jeremiah
 # @Date:   2017-04-22 19:04:12
 # @Last Modified by:   Jeremiah Marks
-# @Last Modified time: 2017-04-22 19:16:46
+# @Last Modified time: 2017-04-22 19:31:08
 
 from db import todoListOperator
 from interface import interactive_text
 
 thisOperator = todoListOperator.todoListOperator()
 todolist = interactive_text.TodoList(thisOperator)
+
+def test():
+	todolist.addTodoItem("This is test item #1!")
+	todolist.addTodoItem("This is another test item!")
+	todolist.printTodoList()
+
+if __name__ == '__main__':
+	test()
