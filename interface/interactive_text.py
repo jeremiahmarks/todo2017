e457dec40aa42a4ad74ba2c86569bc9cd9de825f
Jeremiah Marks <jeremiah@jlmarks.org>
2017-04-22 18:36:47 -0700

    Add timestamps to object
    Basically so it can remember when it was added and when it was modified.

--- interface/interactive_text.py
+++ interface/interactive_text.py
@@ -2,8 +2,16 @@
 # @Author: Jeremiah
 # @Date:   2017-04-22 15:22:45
 # @Last Modified by:   Jeremiah Marks
+# @Last Modified time: 2017-04-22 18:36:19
 
 class TodoItem(object):
 	"""docstring for TodoItem"""
+	def __init__(self, idnum, itemName, createdStamp, modifiedStamp):
 		super(TodoItem, self).__init__()
 		self.itemName = itemName
+		self.idnum = idnum
+		self.createdStamp = createdStamp
+		self.modifiedStamp = modifiedStamp
+
+
+
