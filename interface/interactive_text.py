c6116d4fef6158ffaed75895ac258caa38e3da2d
Jeremiah Marks <jeremiah@jlmarks.org>
2017-04-22 18:37:49 -0700

    Add width of printed columns
    I know that I am going to need to be able to truncate things that are
    long, this is so I can do that.

--- interface/interactive_text.py
+++ interface/interactive_text.py
@@ -2,7 +2,7 @@
 # @Author: Jeremiah
 # @Date:   2017-04-22 15:22:45
 # @Last Modified by:   Jeremiah Marks
-# @Last Modified time: 2017-04-22 18:36:19
+# @Last Modified time: 2017-04-22 18:36:59
 
 class TodoItem(object):
 	"""docstring for TodoItem"""
@@ -12,6 +12,10 @@ class TodoItem(object):
 		self.idnum = idnum
 		self.createdStamp = createdStamp
 		self.modifiedStamp = modifiedStamp
+		self.nameWidth = 80
+		self.defautWidth = 20
+
+
 
 
 
