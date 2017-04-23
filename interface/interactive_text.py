449a8bf99df9ead216b00a613b7302f60ca4f8f1
Jeremiah Marks <jeremiah@jlmarks.org>
2017-04-22 18:39:00 -0700

    Add toString method
    Basically adjust how the todo item prints so that you can print a pretty
    todo list.

--- interface/interactive_text.py
+++ interface/interactive_text.py
@@ -2,7 +2,7 @@
 # @Author: Jeremiah
 # @Date:   2017-04-22 15:22:45
 # @Last Modified by:   Jeremiah Marks
-# @Last Modified time: 2017-04-22 18:36:59
+# @Last Modified time: 2017-04-22 18:38:00
 
 class TodoItem(object):
 	"""docstring for TodoItem"""
@@ -14,6 +14,23 @@ class TodoItem(object):
 		self.modifiedStamp = modifiedStamp
 		self.nameWidth = 80
 		self.defautWidth = 20
+
+	def __str__(self):
+		blockedName = []
+		tempname = self.itemName
+		while (len(tempname) > 0):
+			if len(tempname) <= self.nameWidth:
+				while len(tempname) < self.nameWidth:
+					tempname += " "
+				blockedName.append(tempname[:])
+				tempname = ''
+			else:
+				blockedName.append(tempname[:nameWidth])
+				tempname =  tempname[nameWidth:]
+		retstr = "%04d|  " %(self.idnum, )
+		retstr = retstr + ''.join(blockedName[0])
+		retstr = retstr + "|  " +self.createdStamp
+		return retstr
 
 
 
